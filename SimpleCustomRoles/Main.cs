2f5eb8e105ab9730ad2bd5b51e9e936271971670
SlejmUr <slejmur@protonmail.com>
2025-06-17 15:22:17 +0200

    Add proper 0.5.3

--- SimpleCustomRoles/Main.cs
+++ SimpleCustomRoles/Main.cs
@@ -20,7 +20,7 @@ internal class Main : Plugin<Config>
     #region Plugin Info
     public override string Author => "SlejmUr";
     public override string Name => "SimpleCustomRoles";
-    public override Version Version => new(0, 5, 2);
+    public override Version Version => new(0, 5, 3);
     public override string Description => "Add simple YAML Support for creating custom roles.";
     public override Version RequiredApiVersion => new(LabApiProperties.CompiledVersion);
     #endregion
